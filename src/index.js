686649396a67d56eb18855e88accf1ae8a32b3f1
JotaJotaM1 <johnjames_89@hotmail.com>
2024-01-21 21:47:21 -0500

    finished project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,59 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+console.log("Happy hacking :)------");
 
-console.log('Happy hacking :)')
+//web api
+const baseUrl = "https://platzi-avo.vercel.app";
+
+const appNode = document.querySelector("#app");
+
+const formatPrice = (price) => {
+  const newPrice = new window.Intl.NumberFormat("en-En", {
+    style: "currency",
+    currency: "USD",
+  }).format(price)
+
+  return newPrice;
+};
+
+// conectarnos al server
+window
+  .fetch(`${baseUrl}/api/avo`)
+
+  // procesar la respuesta y convertirla en JSON
+  .then((respuesta) => respuesta.json())
+
+  // JSON -> Data -> Renderizar info al browser
+  .then((responseJson) => {
+    const todosLosItems = [];
+
+    responseJson.data.forEach((item) => {
+      //crear la imagen
+      const imagen = document.createElement("img");
+      imagen.src = `${baseUrl}${item.image}`;
+      imagen.className = "img-avocado";
+
+      //crear titulo
+      const title = document.createElement("h2");
+      title.textContent = item.name;
+      title.className = "tiitleH2";
+
+      //crear precio
+      const price = document.createElement("p");
+      price.textContent = formatPrice(item.price);
+      price.className = "priceAvocado";
+
+      const buttonAdd = document.createElement("button");
+      buttonAdd.textContent = "Add to Cart";
+      buttonAdd.className = "btnAdd"
+
+      // creamos el contenedor div y le agregarmos imagen, title y price
+      const container = document.createElement("div");
+      container.append(imagen, title, price, buttonAdd);
+      container.className = "cardContainer";
+      
+
+      //Hacemos push a todos los items dentro de container
+      todosLosItems.push(container);
+    });
+
+    appNode.append(...todosLosItems);
+  });
